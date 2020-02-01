7aab3c318c0993c2b3ef910c5e9e59d4aeaef914
Nathaniel Cook <nvcook42@gmail.com>
2020-02-01 13:27:00 -0700

    feat(wasm): explore cranelift

---
+++ wasm/src/jim.rs
@@ -0,0 +1,118 @@
+extern crate cranelift;
+extern crate cranelift_module;
+extern crate cranelift_simplejit;
+extern crate peg;
+
+use std::mem;
+use std::process;
+
+mod frontend;
+mod jit;
+
+fn main() {
+    // Create the JIT instance, which manages all generated functions and data.
+    let mut jit = jit::JIT::new();
+
+    // A small test function.
+    let foo_code = "set foo bar";
+
+    //// Pass the string to the JIT, and it returns a raw pointer to machine code.
+    let foo = jit.compile(&foo_code).unwrap_or_else(|msg| {
+        eprintln!("error: {}", msg);
+        process::exit(1);
+    });
+
+    // Cast the raw pointer to a typed function pointer. This is unsafe, because
+    // this is the critical point where you have to trust that the generated code
+    // is safe to be called.
+    //
+    // TODO: Is there a way to fold this transmute into `compile` above?
+    let foo = unsafe { mem::transmute::<_, fn() -> isize>(foo) };
+
+    //// And now we can call it!
+    println!("the answer is: {}", foo());
+
+    //// -------------------------------------------------------------------------//
+
+    //// Another example: Recursive fibonacci.
+    //let recursive_fib_code = "\
+    //    fn recursive_fib(n) -> (r) {
+    //        r = if n == 0 {
+    //                 0
+    //            } else {
+    //                if n == 1 {
+    //                    1
+    //                } else {
+    //                    recursive_fib(n - 1) + recursive_fib(n - 2)
+    //                }
+    //            }
+    //    }
+    //";
+
+    //// Same as above.
+    //let recursive_fib = jit.compile(&recursive_fib_code).unwrap_or_else(|msg| {
+    //    eprintln!("error: {}", msg);
+    //    process::exit(1);
+    //});
+    //let recursive_fib = unsafe { mem::transmute::<_, fn(isize) -> isize>(recursive_fib) };
+
+    //// And we can now call it!
+    //println!("recursive_fib(10) = {}", recursive_fib(10));
+
+    //// -------------------------------------------------------------------------//
+
+    //// Another example: Iterative fibonacci.
+    //let iterative_fib_code = "\
+    //    fn iterative_fib(n) -> (r) {
+    //        if n == 0 {
+    //            r = 0
+    //        } else {
+    //            n = n - 1
+    //            a = 0
+    //            r = 1
+    //            while n != 0 {
+    //                t = r
+    //                r = r + a
+    //                a = t
+    //                n = n - 1
+    //            }
+    //        }
+    //    }
+    //";
+
+    //// Same as above.
+    //let iterative_fib = jit.compile(&iterative_fib_code).unwrap_or_else(|msg| {
+    //    eprintln!("error: {}", msg);
+    //    process::exit(1);
+    //});
+    //let iterative_fib = unsafe { mem::transmute::<_, fn(isize) -> isize>(iterative_fib) };
+
+    //// And we can now call it!
+    //println!("iterative_fib(10) = {}", iterative_fib(10));
+
+    //// -------------------------------------------------------------------------//
+
+    //// Let's say hello, by calling into libc. The puts function is resolved by
+    //// dlsym to the libc function, and the string &hello_string is defined below.
+    //let hello_code = "\
+    //    fn hello() -> (r) {
+    //        puts(&hello_string)
+    //    }
+    //";
+
+    //jit.create_data("hello_string", "hello world!\0".as_bytes().to_vec())
+    //    .unwrap_or_else(|msg| {
+    //        eprintln!("error: {}", msg);
+    //        process::exit(1);
+    //    });
+
+    //// Same as above.
+    //let hello = jit.compile(&hello_code).unwrap_or_else(|msg| {
+    //    eprintln!("error: {}", msg);
+    //    process::exit(1);
+    //});
+    //let hello = unsafe { mem::transmute::<_, fn() -> isize>(hello) };
+
+    //// And we can now call it!
+    //hello();
+}
